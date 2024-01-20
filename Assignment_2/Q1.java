157901e6e46634cefbc24dabb040835cead67cc5
ANKUSH DUTTA <114818651+AnkushD-11@users.noreply.github.com>
2024-01-20 20:25:43 +0530

    Update Q1.java

--- Assignment_2/Q1.java
+++ Assignment_2/Q1.java
@@ -1,1 +1,11 @@
+package Assignment_2;
+
+public class q1 {
+    public static void main(String[] args) {
+        String[] alpha = {"A", "B", "C", "D", "E"};
+        for (int i = 0; i < alpha.length; i++) {
+            System.out.print(alpha[i]);
+    }
+}
+}
 
